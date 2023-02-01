0989bea1bc19d0e79907e003dc33bf407a0b1ba7
Rahul <rahulsharmacs50@gmail.com>
2023-02-01 21:01:57 +0100

    test

--- tests/test_ms.py
+++ tests/test_ms.py
@@ -2,6 +2,7 @@ from statmath import mathstats, basics, rtsa
 import numpy as np
 import math
 
+#TODO: remove the dummy test
 def test_mse2():
     a = np.array([1, 2, 3, 4, 5])
     b = np.array([6, 7, 8, 9, 10])
