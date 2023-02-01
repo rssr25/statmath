05d7081f0b53dda7cdca3704877f483569a09e87
Rahul <rahulsharmacs50@gmail.com>
2023-02-01 21:03:19 +0100

    dummy test removed

--- tests/test_ms.py
+++ tests/test_ms.py
@@ -1,10 +1,3 @@
 from statmath import mathstats, basics, rtsa
 import numpy as np
 import math
-
-#TODO: remove the dummy test
-def test_mse2():
-    a = np.array([1, 2, 3, 4, 5])
-    b = np.array([6, 7, 8, 9, 10])
-
-    assert rtsa.mse(a, b) == 25
